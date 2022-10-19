454e57c85780baa40157fc5fdeb8dd6bbf2b2113
VLllll VLllll <leevalera92@gmail.com>
2022-10-19 17:23:11 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mass = new int[100];
+            for (int i = 0; i < mass.Length; i++)
+            {
+                Random rnd = new Random();
+                mass[i] = rnd.Next(100);
+            }
+
+            for (int i = 0; i < mass.Length; i++)
+            {
+                Console.WriteLine(mass[i] + " ");
+            }
+
+            int min = int.MaxValue;
+            int max = int.MinValue;
+            for (int i = 0; i < mass.Length; i++)
+            {
+                if (mass[i] > max)
+                {
+                    max = mass[i];
+                }
+                if (mass[i] < min)
+                {
+                    min = mass[i];
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Максимальное число: " + max);
+            Console.WriteLine("Минимальное число: " + min);
+        }
+    }
+}
